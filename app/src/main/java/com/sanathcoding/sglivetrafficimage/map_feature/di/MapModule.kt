32be5f3edc89591763dcbcff317513e9f7a181f5
Sanath <sanath@entrusttitle.com>
2023-02-12 00:35:15 +0530

    setup dependency Injection

--- app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/di/MapModule.kt
+++ app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/di/MapModule.kt
@@ -1,9 +1,13 @@
 package com.sanathcoding.sglivetrafficimage.map_feature.di
 
+import android.app.Application
+import androidx.room.Room
 import com.sanathcoding.sglivetrafficimage.core.common.ConstValue.BASE_URL
+import com.sanathcoding.sglivetrafficimage.map_feature.data.local.CameraDataBase
 import com.sanathcoding.sglivetrafficimage.map_feature.data.remote.TrafficImageApi
 import com.sanathcoding.sglivetrafficimage.map_feature.data.remote.repository.TrafficImageRepositoryImpl
 import com.sanathcoding.sglivetrafficimage.map_feature.domain.repository.TrafficImageRepository
+import com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case.GetTrafficImageUseCase
 import com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case.SearchUseCase
 import dagger.Module
 import dagger.Provides
@@ -19,18 +23,34 @@ object MapModule {
 
     @Provides
     @Singleton
+    fun provideGetTrafficImageUseCase(repository: TrafficImageRepository): GetTrafficImageUseCase {
+        return GetTrafficImageUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrafficImageRepository(api: TrafficImageApi, db: CameraDataBase): TrafficImageRepository {
+        return TrafficImageRepositoryImpl(api, db.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCameraDatabase(app: Application): CameraDataBase {
+        return Room.databaseBuilder(
+            app,
+            CameraDataBase::class.java,
+            "camera.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
     fun provideTrafficImageApi(): TrafficImageApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TrafficImageApi::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideTrafficImageRepository(api: TrafficImageApi): TrafficImageRepository {
-        return TrafficImageRepositoryImpl(api)
     }
 
     @Provides
