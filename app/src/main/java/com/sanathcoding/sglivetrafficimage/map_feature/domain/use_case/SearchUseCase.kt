ff6173a7620cd8de79a5c4c7229f004fc06ba41c
Sanath <sanath@entrusttitle.com>
2023-02-13 10:57:25 +0530

    UseCase Modification

--- app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/domain/use_case/SearchUseCase.kt
+++ app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/domain/use_case/SearchUseCase.kt
@@ -1,33 +1,31 @@
 package com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case
 
+import android.util.Log
 import com.sanathcoding.sglivetrafficimage.R
 import com.sanathcoding.sglivetrafficimage.core.util.UiText
 import com.sanathcoding.sglivetrafficimage.core.util.ValidationResult
 
 class SearchUseCase {
 
-    fun execute(query: String): ValidationResult {
+    fun execute(query: String): Boolean {
         if (query.isBlank()) {
-            return ValidationResult(
-                isSuccessful = false,
-                errorMsg = UiText.StringResource(
-                    resId = R.string.query_blank_error
-                )
-            )
+            return false
         }
-        return ValidationResult(
-            isSuccessful = true
-        )
+        return true
     }
 
     fun doseMatchSearchQuery(query: String): Boolean {
+        Log.d("Camera", "Camera query $query")
         val matchingCombinations = listOf(
             "Camera$query",
             "Camera $query",
-            "Camera ${query.first()}",
+            "$query",
         )
+        Log.d("Camera", "Camera List ${matchingCombinations.any {
+            it.contains(query, true)
+        }}")
         return matchingCombinations.any {
-            it.contains(query)
+            it.contains(query, true)
         }
     }
 }
