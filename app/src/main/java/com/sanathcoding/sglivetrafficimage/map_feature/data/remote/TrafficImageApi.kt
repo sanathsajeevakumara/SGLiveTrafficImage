56e0ff18cedcc89c1e7cec410319e77c769f7f1c
Sanath <sanath@entrusttitle.com>
2023-02-13 10:51:13 +0530

    Added get traffic image by date time api call

--- app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/data/remote/TrafficImageApi.kt
+++ app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/data/remote/TrafficImageApi.kt
@@ -2,6 +2,7 @@ package com.sanathcoding.sglivetrafficimage.map_feature.data.remote
 
 import com.sanathcoding.sglivetrafficimage.map_feature.data.remote.dto.TrafficImageDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TrafficImageApi {
 
@@ -9,5 +10,7 @@ interface TrafficImageApi {
     suspend fun getTrafficImages(): TrafficImageDto
 
     @GET("/v1/transport/traffic-images/{date_time}")
-    suspend fun getTrafficImageByTime(dateTime: String):TrafficImageDto
+    suspend fun getTrafficImageByDateTime(
+        @Path("date_time") dateTime: String
+    ):TrafficImageDto
 }
