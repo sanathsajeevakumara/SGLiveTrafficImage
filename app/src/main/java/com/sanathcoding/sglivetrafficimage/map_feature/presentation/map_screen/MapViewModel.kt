bedd7bbc693415ed1f47c2dd835b79a60aef0fd2
Sanath <sanath@entrusttitle.com>
2023-02-13 11:00:35 +0530

    Added GetTrafficImageByDateTime implementation in mapview model

--- app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/presentation/map_screen/MapViewModel.kt
+++ app/src/main/java/com/sanathcoding/sglivetrafficimage/map_feature/presentation/map_screen/MapViewModel.kt
@@ -1,36 +1,63 @@
 package com.sanathcoding.sglivetrafficimage.map_feature.presentation.map_screen
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sanathcoding.sglivetrafficimage.core.common.Resource
+import com.sanathcoding.sglivetrafficimage.map_feature.domain.model.Camera
+import com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case.GetTrafficImageByDateTimeUseCase
 import com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case.GetTrafficImageUseCase
+import com.sanathcoding.sglivetrafficimage.map_feature.domain.use_case.SearchUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
+import java.net.URLEncoder
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor(
     private val getTrafficImageUseCase: GetTrafficImageUseCase,
-): ViewModel() {
+    private val getTrafficImageByDateTimeUseCase: GetTrafficImageByDateTimeUseCase,
+    private val searchUseCase: SearchUseCase
+) : ViewModel() {
 
     var mapState by mutableStateOf(MapState())
 
-    private val _searchText = MutableStateFlow("")
-    val searchText = _searchText.asStateFlow()
+    var cameras = mutableListOf<Camera>()
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery = _searchQuery.asStateFlow()
 
     private val _isSearching = MutableStateFlow(false)
     val isSearching = _isSearching.asStateFlow()
 
-    private val _mapEvent = MutableSharedFlow<MapEvent>()
-    val mapEvent = _mapEvent.asSharedFlow()
-
-
     init {
         getTrafficImages()
     }
+
+    private val _cameraList = MutableStateFlow(cameras)
+    val cameraList = searchQuery
+        .onEach { _isSearching.update { true } }
+        .combine(_cameraList) { searchQuery, cameraList ->
+            if (searchUseCase.execute(searchQuery)) {
+                cameraList
+            } else {
+                delay(2000L)
+                cameraList.filter {
+//                searchUseCase.doseMatchSearchQuery(searchQuery)
+                    it.cameraId.contains(searchQuery)
+                }
+            }
+        }
+        .onEach { _isSearching.update { true } }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            _cameraList.value
+        )
 
     private fun getTrafficImages() {
         getTrafficImageUseCase().onEach { resource ->
@@ -40,9 +67,6 @@ class MapViewModel @Inject constructor(
                         error = resource.message ?: "An Unexpected error occurred",
                         isLoading = false
                     )
-                    _mapEvent.emit(MapEvent.ShowSnackBar(
-                        resource.message?: "Unknown error"
-                    ))
                 }
                 is Resource.Loading -> {
                     mapState = mapState.copy(
@@ -52,15 +76,56 @@ class MapViewModel @Inject constructor(
                 is Resource.Success -> {
                     mapState = mapState.copy(
                         camera = resource.data ?: emptyList(),
-                        isLoading = false
+                        isLoading = false,
                     )
+                    resource.data?.forEach {
+                        cameras.add(it)
+                    }
+
+
                 }
             }
         }.launchIn(viewModelScope)
     }
 
+    fun getTrafficImageByDateTime(date: String, time: String) {
+
+        val apiDateFormat = date + "T" + time + "Z"
+        Log.d("Map", "API date time : $apiDateFormat")
+        val encodedDateTime = URLEncoder.encode(apiDateFormat, "utf-8")
+        Log.d("Map", "Encoded date time : $encodedDateTime")
+
+        getTrafficImageByDateTimeUseCase(encodedDateTime).onEach { resource ->
+            when (resource) {
+                is Resource.Error -> {
+                    Log.d("Filter data", "On error section")
+                    mapState = mapState.copy(
+                        error = resource.message ?: "An Unexpected error occurred",
+                        isLoading = false
+                    )
+                }
+                is Resource.Loading -> {
+                    Log.d("Filter data", "On Loading section")
+                    mapState = mapState.copy(
+                        isLoading = true
+                    )
+                }
+                is Resource.Success -> {
+                    Log.d("Filter data", "On Success section")
+                    mapState = mapState.copy(
+                        camera = resource.data ?: emptyList(),
+                        isLoading = false,
+                    )
+                    resource.data?.map {
+                        Log.d("Filter data", "Filter Data : $it")
+                    }
+                }
+            }
+        }
+    }
+
     fun onSearchTextChange(text: String) {
-        _searchText.value = text
+        _searchQuery.value = text
     }
 
 }
